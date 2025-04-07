707ad8564faee5aa4498023fad7c2a4bf08ffefc
jeroen-coder <himjero@gmail.com>
2025-04-07 10:01:31 +0200

    Update script.js

--- hoeveel dagen tot/js/script.js
+++ hoeveel dagen tot/js/script.js
@@ -15,7 +15,7 @@ min = 0;
 sec = 0;
 
 if(dagNu > dagEinde || dagNu == dagEinde  && uurNu >= uurEinde && minNu >= minEinde && secNu >= secEinde){
-  document.getElementById("hoelang").innerHTML = "It arrived";
+  document.getElementById("hoelang").innerHTML = "have fun";
 }
 else{
 
@@ -76,7 +76,7 @@ function per_sec() {
           uur == 23;
           dag--;
           if(dag == -1){
-            document.getElementById("hoelang").innerHTML = "It arrived";
+            document.getElementById("hoelang").innerHTML = "have fun";
           }
           else if(dag == 1){
             document.getElementById("dagMv").innerHTML = "dag";
